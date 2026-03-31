d1c314fa02e96ebf3e35b9e8e986aa80dd2d0fb4
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-03-31 10:46:35 +0530

    add flow

--- app/kart/page.tsx
+++ app/kart/page.tsx
@@ -1,43 +1,15 @@
 "use client";
-import { Background, ReactFlow, Controls } from "@xyflow/react";
-import "@xyflow/react/dist/style.css";
+
+import dynamic from "next/dynamic";
+
+const FlowComponent = dynamic(() => import("../../components/kartFlow"), {
+  ssr: false,
+});
 
 export default function Page() {
-  const initialNodes = [
-    {
-      id: "n1",
-      data: { label: "Node 1" },
-      position: { x: 0, y: 0 },
-      type: "input",
-    },
-    {
-      id: "n2",
-      data: { label: "Node 2" },
-      position: { x: 100, y: 100 },
-    },
-  ];
-  const initialEdges = [
-    {
-      id: "n1-n2",
-      source: "n1",
-      target: "n2",
-      label: "connects with",
-      type: "step",
-    },
-  ];
-
   return (
     <div>
-      <ReactFlow
-        width={700}
-        height={700}
-        nodes={initialNodes}
-        edges={initialEdges}
-        fitView
-      >
-        <Background />
-        <Controls />
-      </ReactFlow>
+      <FlowComponent />
     </div>
   );
 }
