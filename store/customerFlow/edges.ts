c09bfd5363904ddc738ee2157c43f14aabf83364
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-03-31 11:48:42 +0530

    add customer payment

--- store/customerFlow/edges.ts
+++ store/customerFlow/edges.ts
@@ -5,6 +5,7 @@ export const initialEdges = [
   { id: "e2-3", source: "customer-login", target: "customer-account" },
   { id: "e3-4", source: "customer-account", target: "customer-add-to-cart" },
   { id: "e4-5", source: "customer-add-to-cart", target: "customer-checkout" },
-  { id: "e5-6", source: "customer-checkout", target: "customer-order" },
-  { id: "e6-7", source: "customer-account", target: "customer-wishlist" },
+  { id: "e5-6", source: "customer-checkout", target: "customer-payment" },
+  { id: "e5-7", source: "customer-payment", target: "customer-order" },
+  { id: "e6-8", source: "customer-account", target: "customer-wishlist" },
 ] as Edge[];
--- store/customerFlow/nodes.ts
+++ store/customerFlow/nodes.ts
@@ -1,39 +1,44 @@
-import { type AppNode } from "./types";
+  import { type AppNode } from "./types";
 
-export const initialNodes = [
-  {
-    id: "customer-register",
-    position: { x: 0, y: 0 },
-    data: { label: "Customer Register" },
-  },
-  {
-    id: "customer-login",
-    position: { x: 0, y: 100 },
-    data: { label: "Customer Login" },
-  },
-  {
-    id: "customer-account",
-    position: { x: 0, y: 200 },
-    data: { label: "Customer Account" },
-  },
-  {
-    id: "customer-add-to-cart",
-    position: { x: 0, y: 300 },
-    data: { label: "Customer Add to Cart" },
-  },
-  {
-    id: "customer-checkout",
-    position: { x: 0, y: 400 },
-    data: { label: "Customer Checkout" },
-  },
-  {
-    id: "customer-order",
-    position: { x: 200, y: 0 },
-    data: { label: "Customer Order" },
-  },
-  {
-    id: "customer-wishlist",
-    position: { x: 200, y: 100 },
-    data: { label: "Customer Wishlist" },
-  },
-] as AppNode[];
+  export const initialNodes = [
+    {
+      id: "customer-register",
+      position: { x: 0, y: 0 },
+      data: { label: "Customer Register" },
+    },
+    {
+      id: "customer-login",
+      position: { x: 0, y: 100 },
+      data: { label: "Customer Login" },
+    },
+    {
+      id: "customer-account",
+      position: { x: 0, y: 200 },
+      data: { label: "Customer Account" },
+    },
+    {
+      id: "customer-add-to-cart",
+      position: { x: 0, y: 300 },
+      data: { label: "Customer Add to Cart" },
+    },
+    {
+      id: "customer-checkout",
+      position: { x: 0, y: 400 },
+      data: { label: "Customer Checkout" },
+    },
+    {
+      id: "customer-payment",
+      position: { x: 0, y: 500 },
+      data: { label: "Customer Payment" },
+    },
+    {
+      id: "customer-order",
+      position: { x: 200, y: 0 },
+      data: { label: "Customer Order" },
+    },
+    {
+      id: "customer-wishlist",
+      position: { x: 200, y: 100 },
+      data: { label: "Customer Wishlist" },
+    },
+  ] as AppNode[];
