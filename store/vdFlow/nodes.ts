8074343623f6e9ac1506f3c31c67e8c68b2f8198
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-04-01 10:25:56 +0530

    fix type

--- store/vdFlow/nodes.ts
+++ store/vdFlow/nodes.ts
@@ -4,21 +4,21 @@ export const initialNodes = [
   {
     id: "vd-flow-node-1",
     position: { x: 800, y: 300 },
-    data: { label: "received parcel from mini hub" },
+    data: { label: "Received parcel from mini hub" },
   },
   {
     id: "vd-flow-node-2",
     position: { x: 800, y: 400 },
-    data: { label: "delivered to customer" },
+    data: { label: "Delivered to customer" },
   },
   {
     id: "vd-flow-node-3",
     position: { x: 800, y: 500 },
-    data: { label: "customer cancel order" },
+    data: { label: "Customer cancel order" },
   },
   {
     id: "vd-flow-node-4",
     position: { x: 800, y: 600 },
-    data: { label: "return parcel to destination mini hub" },
+    data: { label: "Return parcel to destination mini hub" },
   },
 ] as AppNode[];
