dff4c7f83079f56d4039d002023bbd990f333d20
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-04-06 11:59:39 +0530

    add Payment Logs

--- store/orderPaymentFlow/edges.ts
+++ store/orderPaymentFlow/edges.ts
@@ -19,4 +19,16 @@ export const initialEdges = [
     target: "payment-failed",
     type: "step",
   },
+  {
+    id: "order-payment-edge-4",
+    source: "payment-successful",
+    target: "payment-logs",
+    type: "step",
+  },
+  {
+    id: "order-payment-edge-5",
+    source: "payment-failed",
+    target: "payment-logs",
+    type: "step",
+  },
 ] as Edge[];
